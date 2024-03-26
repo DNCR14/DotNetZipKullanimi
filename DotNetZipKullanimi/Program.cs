06e1e56404a1312274c3a4e5b6f28583e7dcb58d
DNCR <DNCR@DESKTOP-NGHV15S>
2024-03-26 23:50:56 +0300

    Add project files.

---
+++ DotNetZipKullanimi/Program.cs
@@ -0,0 +1,33 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetZipKullanimi
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string kaynakKlasor = @"C:\_buraya_kaynak_klasor_gelecek";
+            string ziplenmisDosya = @"C:\_buraya_ziplenmis_dosya_gelecek";
+
+            // Klasörü arşivle
+            ZipKlasor(kaynakKlasor, ziplenmisDosya);
+
+            Console.WriteLine("Arşivleme işlemi tamamlandı.");
+        }
+
+        static void ZipKlasor(string kaynak, string ziplenmisDosya)
+        {
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddDirectory(kaynak); // Klasörü arşiv dosyasına ekle
+                zip.Password = "Parola";
+                zip.Save(ziplenmisDosya); // Arşiv dosyasını kaydet
+            }
+        }
+    }
+}
